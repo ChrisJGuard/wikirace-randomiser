561795ff782f49da3748400c458fb475b300300b
Chris Guard <christopher.guard@gmail.com>
2021-06-08 17:59:28 +0100

    Add provisional modifiedDay function

--- scripts/randomise.js
+++ scripts/randomise.js
@@ -26,3 +26,37 @@ function regularDay(rounds) {
 
   points.forEach((point, round) => console.log(`Race ${round + 1}: ${point} points`));
 }
+
+function modifiedDay(rounds) {
+  // Create a placeholder string for our results
+  let results = "Here are today's races:";
+
+  // Check to see what is modified
+  let isCycleDay = modifierCheck();
+  let isReverseDay = modifierCheck();
+  let isChainDay = modifierCheck();
+
+  // If it's a cycle day...
+  if (isCycleDay) {
+    // Create an empty array for our cycled names
+    let cycledNames = [];
+
+    // Randomly assign one of the two possible cycles without repetition
+    randomNumber(1,2) === 1 ? cycledNames = ["Molly", "Hugh", "Hywel"] : cycledNames = ["Hywel", "Molly", "Hugh"];
+
+    // Append to our results string
+    results += `\nToday is a cycle day! Hugh scores points for ${cycledNames[0]}. Hywel scores points for ${cycledNames[1]}. Molly scores points for ${cycledNames[2]}.`;
+  } else results += `\nToday is not a cycle day.`
+
+  // If it's a reverse day...
+  if (isReverseDay) {
+    results += `\nToday is a reverse day! I suggest reversing Race ${randomNumber(1,rounds)}.`
+  } else results += `\nToday is not a cycle day.`
+
+  // If it's a chain day...
+  if (isChainDay) {
+    results += `\nToday is a chain day! Don't forget to announce after the first race.`
+  } else results += `\nToday is not a chain day.`
+
+  return results;
+}
