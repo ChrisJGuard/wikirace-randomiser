d80f6a9cd7f3c7e1977a2661a8af3c0d5202a621
Chris Guard <christopher.guard@gmail.com>
2021-06-08 18:15:07 +0100

    Add provisional woodyWoodpeckerDay function

--- scripts/randomise.js
+++ scripts/randomise.js
@@ -46,17 +46,56 @@ function modifiedDay(rounds) {
 
     // Append to our results string
     results += `\nToday is a cycle day! Hugh scores points for ${cycledNames[0]}. Hywel scores points for ${cycledNames[1]}. Molly scores points for ${cycledNames[2]}.`;
-  } else results += `\nToday is not a cycle day.`
+  } else results += `\nToday is not a cycle day.`;
 
   // If it's a reverse day...
   if (isReverseDay) {
-    results += `\nToday is a reverse day! I suggest reversing Race ${randomNumber(1,rounds)}.`
-  } else results += `\nToday is not a cycle day.`
+    results += `\nToday is a reverse day! I suggest reversing Race ${randomNumber(1,rounds)}.`;
+  } else results += `\nToday is not a cycle day.`;
 
   // If it's a chain day...
   if (isChainDay) {
-    results += `\nToday is a chain day! Don't forget to announce after the first race.`
-  } else results += `\nToday is not a chain day.`
+    results += `\nToday is a chain day! Don't forget to announce after the first race.`;
+  } else results += `\nToday is not a chain day.`;
 
   return results;
+}
+
+function woodyWoodpeckerDay(rounds) {
+  // Create a placeholder string for our results
+  let results = "It's Woody Woodpecker Day! Here are today's random rounds and how much they're worth:";
+
+  // Generate an array of points
+  let points = generatePoints(rounds);
+
+  // Generate an array of round selections
+  let roundTypes = [];
+
+  for (i = 0; i < rounds; i++) {
+    roundTypes.push(randomNumber(1,5));
+  }
+
+  // Map these to outputted round types and append to the results
+  roundTypes.forEach((type, round) => {
+    switch (type) {
+      case 1:
+        results += `\nRound ${round + 1}: Regular Round (${points[round]} points).`
+        break;
+      case 2:
+        results += `\nRound ${round + 1}: Pursuit Round (${points[round]} points).`
+        break;
+      case 3:
+        results += `\nRound ${round + 1}: Countdown Round (${points[round]} points).`
+        break;
+      case 4:
+        results += `\nRound ${round + 1}: TENET Round (${points[round]} points).`
+        break;
+      case 5:
+        results += `\nRound ${round + 1}: Chaos Round (${points[round]} points).`
+        break;
+    }
+  })
+
+  return results;
+
 }
