3673dd003fd4222bb0d56b710a216f8e0e71ca37
Chris Guard <christopher.guard@gmail.com>
2021-06-08 16:00:31 +0100

    Add regularRoundArray function

--- scripts/randomise.js
+++ scripts/randomise.js
@@ -3,3 +3,16 @@
 function randomNumber(lower, upper) {
   return Math.floor(Math.random() * (upper - lower + 1) + lower);
 }
+
+function regularRoundArray(rounds) {
+  // Create an empty array to store round points values
+  let points = [];
+
+  // Add randomly generated points value to array for number of rounds
+  for (i = 0; i < rounds; i++) {
+    points.push(randomNumber(1,3));
+  }
+
+  // Return the final array
+  return points;
+}
